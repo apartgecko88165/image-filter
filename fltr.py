fc8edf78927b8e8bafb2fbb5fc3627d0c32b9a75
apartgecko88165 <86565740+apartgecko88165@users.noreply.github.com>
2022-03-28 10:20:31 -0700

    Update fltr.py

--- fltr.py
+++ fltr.py
@@ -1,18 +1,58 @@
 # import 'tkinter' elements for GUI
 import tkinter as tk
-from turtle import color
+from tkinter import filedialog
+from tkinter.simpledialog import askstring
 
 # import pillow for image handling
-from PIL import Image, ImageTk, ImageFilter
+from PIL import Image, ImageTk
 
 
 class Filter(tk.Tk):
+
+    def _green(self, image):
+        # Creates an ImageCore Object from original image
+        pixels = image.getdata()
+        # Creates empty array to hold new pixel values
+        new_pixels = [] 
+        # For every pixel from our original image, it saves
+        # a copy of that pixel to our new_pixels array
+        for p in pixels:
+            new_pixels.append(p)
+        # Starts at the first pixel in the image
+        location = 0
+        # Continues until it has looped through all pixels
+        while location < len(new_pixels):
+            # Gets the current color of the pixel at location
+            p = new_pixels[location]
+            # Splits color into red, green and blue components
+            r = p[0]
+            g = p[1]
+            b = p[2]
+            # Perform pixel manipulation and stores results
+            # to a new red, green and blue components
+            newr = g
+            newg = g
+            newb = g
+
+            new_pixels[location] = (newr, newg, newb)
+            # Changes the location to the next pixel in array
+            location = location + 1
+        # Creates a new image, the same size as the original 
+        # using RGB value format
+        new_image = Image.new("RGB", image.size)
+        # Assigns the new pixel values to newImage
+        new_image.putdata(new_pixels)
+        # Sends the newImage back to the main portion of program
+        self.lineup.append("Greenscale")
+        return new_image
+    
+
 
     def _grey(self, image):
         # Creates an ImageCore Object from original image
         pixels = image.getdata()
         # Creates empty array to hold new pixel values
-        new_pixels = []
+        new_pixels = [] 
         # For every pixel from our original image, it saves
         # a copy of that pixel to our new_pixels array
         for p in pixels:
@@ -32,8 +72,7 @@ class Filter(tk.Tk):
             newr = (r + g + b) // 3
             newg = (r + g + b) // 3
             newb = (r + g + b) // 3
-            # Assign new red, green and blue components to pixel
-            # at that specific location
+
             new_pixels[location] = (newr, newg, newb)
             # Changes the location to the next pixel in array
             location = location + 1
@@ -43,6 +82,7 @@ class Filter(tk.Tk):
         # Assigns the new pixel values to newImage
         new_image.putdata(new_pixels)
         # Sends the newImage back to the main portion of program
+        self.lineup.append("Grayscale")
         return new_image
 
             
@@ -54,47 +94,81 @@ class Filter(tk.Tk):
         
         display = image.resize((width, int(height)), Image.ANTIALIAS)
         
-        self.display = display
+        self.disp = display
         self.image = image
         self.photo = ImageTk.PhotoImage(display)
 
-    def _show(self, label, clicked):
-        label.config(text=clicked.get())
+    def _save(self, image):
+        fname = askstring("File name", "Enter the name for your file")
+        directory = filedialog.askdirectory()
+
+        image.save(f"{directory}/{fname}.png")
+
+    def _show(self, ph, clicked):
+        ph.config(text=clicked.get())
 
 
-        if clicked.get() == "Grayscale":
-            display = self._resize(self.image)
+        if self.clicked.get() == "Grayscale":
+            self._resize(self.image)
 
-            filtered = self._grey(display)
+            self.disp = self._grey(self.disp)
 
-            display = ImageTk.PhotoImage(filtered)
+            #filtered.show()
 
-            tk.Label(self, image=display).grid(row=0, column=0)
+            self.disp.save("temp/photo.png")
+
+            display = ImageTk.PhotoImage(file="temp/photo.png")
+
+            self.canvas.configure(image=display)
+            self.canvas.image = display
+        elif self.clicked.get() == "Greenscale":
+            self._resize(self.image)
+
+            self.disp = self._green(self.disp)
+
+            #filtered.show()
+
+            self.disp.save("temp/photo.png")
+
+            display = ImageTk.PhotoImage(file="temp/photo.png")
+
+            self.canvas.configure(image=display)
+            self.canvas.image = display
 
     def start(self, image):
+
+        self.original = image
+
+        self.lineup = []
+
         self.title("Image Filter")
         
         self.geometry(f"{self.winfo_screenwidth()}x{self.winfo_screenheight()}")
+        self.attributes("-fullscreen",True)
 
         self._resize(image)
         
         # Dropdown menu options
         filters = [
-            "Grayscale"
+            "Grayscale",
+            "Greenscale"
         ]
         
         # datatype of menu text
-        clicked = tk.StringVar()
+        self.clicked = tk.StringVar()
         
         # initial dropdown text
-        clicked.set("Select a Filter")
+        self.clicked.set("Select a Filter")
 
-        tk.Label(self, image=self.photo).grid(row=0, column=0)
+        self.canvas = tk.Label(self, image=self.photo)
+        self.canvas.place(x=0, y=0)
         
         # create menu
-        tk.OptionMenu(self , clicked , *filters).place(x=600)
-        label = tk.Label(self , text=" ")
-        tk.Button(self, text="APPLY", command=lambda: self._show(label, clicked)).place(x=500, y=1)
+        tk.OptionMenu(self , self.clicked , *filters).place(x=600)
+        self.ph = tk.Label(self , text=" ")
+        tk.Button(self, text="APPLY", command=lambda: self._show(self.ph, self.clicked)).place(x=500, y=1)
+        tk.Button(self, text="SAVE", command=lambda: self._save(self.image)).place(x=self.disp.width-40, y=self.disp.height-30)
+
         tk.Label(self, text=f"{self.image.width}x{self.image.height}").place(x=0, y=0)
 
         self.mainloop()
